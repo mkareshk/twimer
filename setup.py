5ab54a878150e4033bba7c1cf4ca019617833bd5
Moein Kareshk <mkareshk@outlook.com>
2021-06-26 20:14:05 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,24 @@
 import setuptools
 
-with open('README.md', 'r') as file:
+with open("README.md", "r") as file:
     long_description = file.read()
 
-with open('requirements.txt', 'r') as file:
-    requirements = file.readlines()
 
 setuptools.setup(
-    name='Twimer', # Replace with your own username
-    version='0.0.3',
-    author='Moein Owhadi Kareshk',
-    author_email='moein.owhadi@gmail.com',
-    description='Stream Tweets into Your Favorite Databases',
+    name="Twimer",  # Replace with your own username
+    version="0.0.4",
+    author="Moein Kareshk",
+    author_email="mkareshk@outlook.com",
+    description="Stream Tweets into Your Favorite Databases",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/owhadi/twimer',
+    long_description_content_type="text/markdown",
+    url="https://github.com/mkareshk/twimer",
     packages=setuptools.find_packages(),
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    install_requires=requirements,
-    python_requires='>=3.6',
+    install_requires=["pymongo", "tweepy"],
+    python_requires=">=3.8",
 )
