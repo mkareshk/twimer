8c3fdf83661695a23e1233acb3e8ff9ff7f3dfe2
Moein Owhadi Kareshk <mkareshk@outlook.com>
2021-12-19 07:21:04 -0800

    update

--- twimer/twitter_connection.py
+++ twimer/twitter_connection.py
@@ -36,6 +36,7 @@ class TwitterConnection(StreamListener):
         try:
 
             tweet = json.loads(tweet)
+            print(tweet)
 
             # check if it's a retweet
             if not self.include_retweets and tweet["text"].startswith("RT @"):
