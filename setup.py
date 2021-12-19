f6423eb31b247595690a68dea439de1fefd66ceb
Moein Owhadi Kareshk <mkareshk@outlook.com>
2021-12-19 08:06:08 -0800

    update the dependencies

--- setup.py
+++ setup.py
@@ -19,6 +19,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["pymongo", "tweepy==3.9.0"],
+    install_requires=["pymongo==4.0.1", "tweepy==3.9.0"],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
     python_requires=">=3.8",
 )
